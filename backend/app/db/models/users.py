39b556f804647a3de3dad8155e6d1c89ddff3405
Tahiti18 <35585881+Tahiti18@users.noreply.github.com>
2025-09-07 16:33:42 +0300

    Update users.py

--- backend/app/db/models/users.py
+++ backend/app/db/models/users.py
@@ -1,11 +1,12 @@
-from sqlalchemy import String, Boolean, Text
+from datetime import datetime
+from sqlalchemy import String, Boolean, Text, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.sql import func
-from sqlalchemy import DateTime
 from ..base import Base
 
 class User(Base):
     __tablename__ = "users"
+
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     email: Mapped[str] = mapped_column(String(255), unique=True, index=True, nullable=False)
     name: Mapped[str | None] = mapped_column(String(255), nullable=True)
@@ -13,5 +14,6 @@ class User(Base):
     timezone: Mapped[str] = mapped_column(String(64), default="Asia/Nicosia", nullable=False)
     email_verified: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
     password_hash: Mapped[str] = mapped_column(Text, nullable=False)
-    created_at: Mapped = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
-    updated_at: Mapped = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
